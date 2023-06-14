f8dacc4d5ed86c4dc43c235a1fe860fa1974c66d
David <daviddadiomov@gmail.com>
2023-06-14 05:15:27 -0700

    first commit

--- src/tuiter/services/tuits-service.js
+++ src/tuiter/services/tuits-service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_BASE = "https://idyllic-sunshine-7e8711.netlify.app";//process.env.REACT_APP_API_BASE || "http://localhost:4000/api";
+const API_BASE = "https://tuiter-node-server-app-pg0i.onrender.com/api";//process.env.REACT_APP_API_BASE || "http://localhost:4000/api";
 const TUITS_API = `${API_BASE}/tuits`;
 
 console.log(API_BASE);
